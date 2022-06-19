a147276a7caf8537857eb0e1be50c22535be29be
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-06-19 09:45:07 +0530

    feat: added props.theme support to stroybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,11 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+
+const themes = [theme];
+addDecorator(withThemesProvider(themes), ThemeProvider);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +14,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
