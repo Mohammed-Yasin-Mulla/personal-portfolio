6e506289194e511879edd3764325416f78ce53aa
Mohammed Yasin Mulla <yasin@theflurn.com>
2022-06-26 12:18:21 +0530

    feat: added technologies comp

--- components/Technologies/Technologies.tsx
+++ components/Technologies/Technologies.tsx
@@ -1,15 +1,38 @@
+import { motion } from "framer-motion";
 import React from "react";
+import styled from "styled-components";
+import TitleSection from "../Common/TitleSection";
 
-interface Props {
-  children: React.ReactNode;
-  isMobile?: boolean;
-}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  max-width: 1080px;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    justify-content: flex-start;
+  }
+`;
 
-export default function Technologies(props: Props) {
-  const { children } = props;
+const AboutText = styled(motion.p)`
+  font-size: 1rem;
+  text-align: center;
+  width: 100%;
+  font-family: ${(props) => props.theme.fonts.primary};
+  font-weight: 300;
+  color: ${(props) => props.theme.colors.slate};
+  margin: 0;
+`;
+
+export default function Technologies() {
   return (
-    <div style={{ height: "50vh", color: "white", padding: "0 150px" }}>
-      {children}
-    </div>
+    <Wrapper>
+      <TitleSection number="02" title="Technologies"></TitleSection>
+      <AboutText>
+        These are some of the technologies and libraries I have been working
+        recently with
+      </AboutText>
+    </Wrapper>
   );
 }
